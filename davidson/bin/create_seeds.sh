a990fd4b299cd5c1cd823d40da1a5dc3522c2303
brwnj <brwnjm@gmail.com>
2013-02-04 11:16:11 -0700

    init

---
+++ davidson/bin/create_seeds.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#BSUB -J find_seeds[1-6]
+#BSUB -e find_seeds.%J.%I.err
+#BSUB -o find_seeds.%J.%I.out
+#BSUB -q normal
+#BSUB -R "select[mem>2] rusage[mem=2] span[hosts=1]"
+#BSUB -n 1
+
+<<DOC
+create tags and find seeds among the reads.
+DOC
+
+set -o nounset -o pipefail -o errexit -x
+
+sample=$LSB_JOBINDEX
+
+data=$HOME/projects/davidson/data
+tags=$data/trbv.tags.fa
+reads=$data/20120924/${sample}_*.trm.fq.gz
+len=35
+bin=$HOME/devel/iSSAKE
+seeds=$data/${sample}_${len}_seeds.fa.gz
+
+python $bin/create_tags.py -v -l $len $tags \
+    | python $bin/find_seeds.py -v - $reads \
+    | gzip -c > $seeds
