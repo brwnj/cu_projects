a4ed9f70544ff5add448f4fa18e4d03ce1f10f21
brwnj <brwnjm@gmail.com>
2013-02-06 16:09:44 -0700

    tags to regions

--- davidson/bin/create_seeds.sh
+++ davidson/bin/create_seeds.sh
@@ -14,13 +14,14 @@ set -o nounset -o pipefail -o errexit -x
 
 sample=$LSB_JOBINDEX
 
+regions=(idx0 trav.fa trbv.fa trav.fa trbv.fa trav.fa trbv.fa)
 bin=$HOME/devel/iSSAKE
 len=35
-data=$HOME/projects/davidson/data
-tags=$data/trbv.tags.fa
-reads=$data/20120924/$sample.trm.fa.gz
-seeds=$data/20120924/$sample.$len.seeds.fa.gz
+data=$HOME/projects/davidson/data/20120924
+region=$data/${regions[$LSB_JOBINDEX]}
+reads=$data/$sample.trm.fa.gz
+seeds=$data/$sample.$len.seeds.fa.gz
 
-python $bin/create_tags.py -v -l $len $tags \
+python $bin/create_tags.py -v -l $len $region \
     | python $bin/find_seeds.py -v - $reads \
     | gzip -c > $seeds
