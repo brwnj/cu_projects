925e75f0093366bdcb94afdf8d78e671a6e606e9
brwnj <brwnjm@gmail.com>
2013-02-08 12:49:39 -0700

    added sort and filter

--- walter/bin/qvalues.sh
+++ walter/bin/qvalues.sh
@@ -16,10 +16,15 @@ samples=(E1T1_Inf E1T1_Uninf E1T24_Inf E1T24_Uninf E1T2_Inf E1T2_Uninf
             E1T8_Inf E1T8_Uninf E2T1_Inf E2T1_Uninf E2T24_Inf E2T24_Uninf
             E2T2_Inf E2T2_Uninf E2T8_Inf E2T8_Uninf E3T1_Inf E3T1_Uninf
             E3T24_Inf E3T24_Uninf E3T2_Inf E3T2_Uninf E3T8_Inf E3T8_Uninf)
-sample=${samples[$(expr $LSB_JOBINDEX - 1)]}
+sample=${samples[$(($LSB_JOBINDEX - 1))]}
 
-peaks=$HOME/projects/walter/results/common/$sample/$sample.novo.peaks.bed.gz
-nullpeaks=$HOME/projects/walter/results/common/$sample/$sample.novo.peaks.shuffle.bed.gz
-qvalues=$HOME/projects/walter/results/common/$sample/$sample.novo.qvalues.gz
+data=$HOME/projects/walter/results/common
+peaks=$data/$sample/$sample.tb2hg19.peaks.bed.gz
+nullpeaks=$data/$sample/$sample.tb2hg19.peaks.shuffle.bed.gz
+qvalues=$data/$sample/$sample.tb2hg19.qvalues.gz
 
-peaktools-qvalues $realpeaks $nullpeaks | gzip -c > $qvalues
+peaktools-qvalues $peaks $nullpeaks \
+    | sortBed -i - \
+    | awk '$7<0.05' \
+    | gzip -c \
+    > $qvalues
