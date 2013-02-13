feee12d6ec4f62dbfea177bf25199e13a9665626
brwnj <brwnjm@gmail.com>
2013-02-13 15:20:04 -0700

    new fasta and fastq parsers

--- utils/parsers.py
+++ utils/parsers.py
@@ -1,36 +1,31 @@
+#! /usr/bin/env python
 from toolshed import nopen
 
-class FastqReader(object):
-    """Yields name, seq, qual."""
-    def __init__(self, fastq):
-        self.fastq = nopen(fastq)
-
-    def __iter__(self):
-        fq = self.fastq
-        while True:
-            id1 = fq.next().strip()
-            seq = fq.next().strip()
-            id2 = fq.next().strip()
-            qual = fq.next().strip()
-            if qual == "":
-                if id1 != "":
-                    sys.stderr.write(">> Incomplete record... skipping.\n")
-                break
-            yield id1[1:], seq, qual
+def read_fastq(fq):
+    fh = nopen(fq)
+    while True:
+        values = list(itertools.islice(fh, 4))
+        if len(values) == 4:
+            id1, seq, id2, qual = values
+        elif len(values) == 0:
+            raise StopIteration
+        else:
+            raise EOFError("unexpected end of file")
+        assert id1.startswith('@'),\
+                ">> Fastq out of sync at read:\n%s\n" % id1
+        assert id2.startswith('+'),\
+                ">> Fastq out of sync at read:\n%s\n" % id1
+        assert len(seq) == len(qual),\
+                ">> Sequence and Quality are not the same length \
+                for read:\n%s\n" % id1
+        yield id1[1:-1], seq[:-1], qual[:-1]
 
 def read_fasta(fa):
-    r"""yields name and seq from fasta.
-    
-    with open('f.fasta') as fp:
-        for name, seq in read_fasta(fp):
-            print(name, seq)
-    """
-    name, seq = None, []
-    for line in fa:
-        line = line.rstrip()
-        if line.startswith('>'):
-            if name: yield (name, ''.join(seq))
-            name, seq = line.lstrip('>'), []
+    fh = nopen(fa)
+    for header, group in itertools.groupby(fh, lambda line: line[0] == '>'):
+        if header:
+            line = group.next()
+            name = line[1:].strip()
         else:
-            seq.append(line)
-    if name: yield (name, ''.join(seq))
+            seq = ''.join(line.strip() for line in group)
+            yield name, seq
