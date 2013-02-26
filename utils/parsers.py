d142b32bb0d648678d88ad94901cf793794150a2
brwnj <brwnjm@gmail.com>
2013-02-26 16:50:57 -0700

    write_fasta method added

--- utils/parsers.py
+++ utils/parsers.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
+from itertools import islice, groupby
 from toolshed import nopen
 
 def read_fastq(fq):
     fh = nopen(fq)
     while True:
-        values = list(itertools.islice(fh, 4))
+        values = list(islice(fh, 4))
         if len(values) == 4:
             id1, seq, id2, qual = values
         elif len(values) == 0:
@@ -22,10 +23,15 @@ def read_fastq(fq):
 
 def read_fasta(fa):
     fh = nopen(fa)
-    for header, group in itertools.groupby(fh, lambda line: line[0] == '>'):
+    for header, group in groupby(fh, lambda line: line[0] == '>'):
         if header:
             line = group.next()
             name = line[1:].strip()
         else:
             seq = ''.join(line.strip() for line in group)
             yield name, seq
+
+def write_fasta(name, seq, wrap=70):
+    print ">%s" % name
+    # sequence lines standardized at length of wrap
+    print "\n".join([seq[i:i + wrap] for i in range(0, len(seq), wrap)])
