d31499e4e0a476225788dd482b737f9435e71318
brwnj <brwnjm@gmail.com>
2013-02-27 13:13:17 -0700

    added plotting

--- shaikh/bin/2_conifer_analyze.sh
+++ shaikh/bin/2_conifer_analyze.sh
@@ -5,6 +5,7 @@
 #BSUB -R "select[mem>16] rusage[mem=16] span[hosts=1]"
 #BSUB -n 1
 #BSUB -P shaikh
+#BSUB -q short
 
 <<doc
 run conifer on tamim's samples
@@ -17,17 +18,39 @@ results=$HOME/projects/shaikh/results/common
 if [[ ! -d $results/analysis ]]; then
     mkdir -p $results/analysis
 fi
+if [[ ! -d $results/analysis/plots ]]; then
+    mkdir -p $results/analysis/plots
+fi
 bin=$HOME/opt/conifer_v0.2.2
 
-python $bin/conifer.py analyze \
-    --probes $bin/probes.txt \
-    --rpkm_dir $results/rpkm \
-    --output $results/analysis/trios.hdf5 \
-    --write_svals $results/analysis/trios_singular_values.txt \
-    --write_sd $results/analysis/trios_sd_values.txt
+probes=$bin/probes.txt
+hdf5=$results/analysis/trios.hdf5
+sing_vals=$results/analysis/trios_singular_values.txt
+sd_vals=$results/analysis/trios_sd_values.txt
 
+calls=$results/analysis/trios_calls.txt
 
-import matplotlib.pyplot as plt
-import pylab as P
-from matplotlib.lines import Line2D
-from matplotlib.patches import Rectangle
+# create the hdf5
+if [[ ! -f $hdf5 ]]; then
+    # first you should plot screeplot to find svd cutoff
+    python $bin/conifer.py analyze \
+        --probes $probes \
+        --rpkm_dir $results/rpkm \
+        --svd 2 \
+        --output $hdf5 \
+        --write_svals $sing_vals \
+        --write_sd $sd_vals
+fi
+
+# do the testing
+if [[ ! -f $calls ]]; then
+    python $bin/conifer.py call \
+        --input $hdf5 \
+        --output $calls
+fi
+
+# create plots
+python $bin/conifer.py plotcalls \
+    --input $hdf5 \
+    --calls $calls \
+    --outputdir $results/analysis/plots
