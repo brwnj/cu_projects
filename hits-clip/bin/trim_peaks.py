82b1d75c1411482cad4b11c4967e42712f099e43
brwnj <brwnjm@gmail.com>
2013-03-01 13:43:42 -0700

    disaster

--- hits-clip/bin/trim_peaks.py
+++ hits-clip/bin/trim_peaks.py
@@ -4,44 +4,42 @@
 Trim peaks by finding the point of maximum intensity then expanding to the
 size of width.
 """
-import argparse
+
 import os
 import sys
 from genomedata import Genome
 from pybedtools import BedTool
 from numpy import nansum
 
-
-def trim_peaks(bed, genomedata, track, chrom, width):
-    """For each peak, find mid then write new coordinates from the mid.
-    """
+def trim_peaks(bed, genomedata, track, chrom, width, verbose):
+    """For each peak, find mid then write new coordinates from the mid."""
     bed = BedTool(bed)
+
+    # ideally this would look more like
+    # for chromosome in genome:
+    #     for supercontig, continuous in chromosome.itercontinuous():
+    # but i've noticed data not being pulled out accurately
     
     with Genome(genomedata) as genome:
         for b in bed:
             # parallelize by chrom if supplied
             if chrom and b.chrom != chrom: continue
-            
+            # data accessor from genomedata
             chromosome = genome[b.chrom]
-            
             # intensities across specified tracks for peak region
             intensities = chromosome[b.start:b.stop, track]
+            # print intensities
             mid = get_mid(b.chrom, b.start, intensities)
-            
-            # calling intensities for some regions were returning all 'nan'
-            if not mid: continue
-            
+
+            # if not mid: continue
             start = mid - width / 2
             stop = mid + width / 2
-            
             # output new peak coordinates
-            fields = (b.chrom, start, stop)
+            fields = (b.chrom, start, stop, b.name, b.score, b.strand)
             print '\t'.join(map(str, fields))
 
-
 def get_mid(chrom, start, intensities):
-    """Finds median position of the maximum intensity value.
-    """
+    """Finds median position of the maximum intensity value."""
     maximum = 0
     maxpositions = []
     
@@ -59,30 +57,34 @@ def get_mid(chrom, start, intensities):
             del maxpositions[0:len(maxpositions)]
             maxpositions.append(idx)
     
-    # get the middle position of the max intensities
+    # return midpoint among the max intensities
     if len(maxpositions) == 0:
         return None
     else:
         midposition = int(maxpositions[len(maxpositions) / 2])
         return midposition
 
+def main(args):
+    kwargs = {'bed':args.peaks,
+              'genomedata':args.genomedata,
+              'track':args.trackname,
+              'chrom':args.chrom,
+              'width':args.width,
+              'verbose':args.verbose}
+    trim_peaks(**kwargs)
 
-def main():
+if __name__ == "__main__":
+    import argparse
     p = argparse.ArgumentParser(description=__doc__, 
                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('peaks', help='peaks file (bed)')
     p.add_argument('genomedata', help="genome data archive")
     p.add_argument('trackname', nargs='+', 
                     help='space delimited track names')
-    p.add_argument("-w", "--width", dest='width', default=60, 
-                    help="full peak width", type=int)
-    p.add_argument("-c", "--chrom", dest='chromosome', default=None, 
+    p.add_argument("-w", "--width", default=60, type=int,
+                    help="full peak width")
+    p.add_argument("-c", "--chrom", default=None, 
                     help="parallelize by specifying chromosome name")
-    
-    args = p.parse_args()
-    trim_peaks(args.peaks, args.genomedata, args.trackname, \
-                    args.chromosome, args.width)
-
-
-if __name__ == "__main__":
-    sys.exit(main())
+    p.add_argument("-v", "--verbose", action='store_true',
+                    help="maximum verbosity")
+    main(p.parse_args())
