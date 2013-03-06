1ed8b92e0fd2d74a3771b703d9ca86b9a8f2cb4a
brwnj <brwnjm@gmail.com>
2013-03-06 12:21:01 -0700

    added concat and demultiplexing

--- polya/bin/ayb.sh
+++ polya/bin/ayb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#BSUB -J ayb
+#BSUB -J ayb_main
 #BSUB -e ayb.%J.err
 #BSUB -o ayb.%J.out
 #BSUB -q normal
@@ -9,34 +9,47 @@
 call bases using AYB
 DOC
 
-set -o nounset -o errexit -x
+set -o nounset -o errexit -o pipefail -x
 
-# lanes=("idx0" "6" "7")
-# lane=${lanes[${LSB_JOBINDEX}]}
 lane=3
 
-# bs=R6I14C36 # barcode is 3'; UMI is 5'
-bs=R6I14C36
+# bs=R6I14C36
+bs=R6I14C86
+# Expected folder structure:
+# $HOME/projects/polya/data/20130305/Data/Intensities/L003/<cycles>/*.cif
 cifs=$HOME/projects/polya/data/20130305
-out=$HOME/projects/polya/results/20130305
+fastq=$cifs/20130226_L00${lane}.fastq.gz
+barcodes=$cifs/barcodes.txt
 
-# AYB -b $bs -d cif -l debug -o $out -p 12 -i $cifs -r L${lane}T1101-2316 --format fastq
-
+jobids=""
 # you need a for loop here because some seg fault
 for (( i = 1101; i < 2320; i++ )); do
     RUNSCRIPT=ayb.${i}.$lane.sh
     echo "#! /usr/bin/env bash" > $RUNSCRIPT
-    echo "#BSUB -J ayb.${i}.$lane" >> $RUNSCRIPT
+    echo "#BSUB -J ayb_slave.${i}.$lane" >> $RUNSCRIPT
     echo "#BSUB -R \"span[hosts=1] select[mem>8] rusage[mem=8]\"" >> $RUNSCRIPT
     echo "#BSUB -e ayb.%J.$lane.err" >> $RUNSCRIPT
     echo "#BSUB -o ayb.%J.$lane.out" >> $RUNSCRIPT
-    echo "#BSUB -n 10" >> $RUNSCRIPT
+    echo "#BSUB -n 8" >> $RUNSCRIPT
     echo "#BSUB -q normal" >> $RUNSCRIPT
     echo "#BSUB -P pillai_kabos_polya" >> $RUNSCRIPT
     echo "
 
-AYB -b $bs -d cif -l debug -o $out -p 10 -i $cifs -r L${lane}T${i} --format fastq s+
+AYB -b $bs -d cif -l debug -o $cifs -p 8 -i $cifs -r L${lane}T${i} --format fastq
 " >> $RUNSCRIPT
-
-    bsub < $RUNSCRIPT
+    
+    # submit the job
+    job=$(bsub < $RUNSCRIPT)
+    # get the id
+    jobid=$(echo $job | sed -rn 's/.*<([0-9]+)>.*/\1/p')
+    # add the job id to a space delimited string
+    jobids="$jobids $jobid"
 done
+
+# wait for completion
+python -m bsub $jobids
+# concatenate into one fastq
+cat $cifs/*.fastq | gzip -c > $fastq
+# demultiplex
+fastq-multx -B $barcodes -m 2 -e $fastq -o %.fastq
+gzip $cifs/*.fastq
