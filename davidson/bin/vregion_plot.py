a4b4002563bf19dcef1ee4e997dc44e0619fb1b9
brwnj <brwnjm@gmail.com>
2013-03-11 16:53:41 -0600

    just print the data

--- davidson/bin/vregion_plot.py
+++ davidson/bin/vregion_plot.py
@@ -6,11 +6,6 @@ from itertools import groupby
 from toolshed import nopen
 from collections import Counter
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import numpy as np
-
 def read_fasta(fa):
     with nopen(fa) as fh:
         for header, group in groupby(fh, lambda line: line[0] == '>'):
@@ -30,34 +25,13 @@ def main(args):
             name = name.replace("size","").replace("cov","").replace("read","").replace("seed:","")
             name_d = dict(zip(name_key, name.split("|")))
             v_regions.update([name_d['vregion']])
-    # stick a header on there
-    # print "v_region\tcoverage"
-    coverage = []
-    regions = []
+    print "v_region\tcoverage"
     for v_region, count in sorted(v_regions.iteritems()):
-        # print "%s\t%s" % (v_region, count)
-        regions.append(v_region)
-        coverage.append(count)
-
-    fig = plt.figure(figsize=(15,10), dpi=300)
-    ax = fig.add_subplot(111)
-    N = np.arange(len(coverage))
-    bar_width = 2
-    ax.bar(N, coverage, facecolor='red')
-    ax.set_ylabel('Count')
-    plt.xticks(rotation=70)
-    ax.set_xticks(N)
-    ax.set_xticklabels(regions)
-    
-    plt.title(args.title)
-    plt.savefig(args.out, bbox_inches='tight', dpi=300)
-    
+        print "%s\t%s" % (v_region, count)
 
 if __name__ == '__main__':
     import argparse
     p = argparse.ArgumentParser(description=__doc__,
                     formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument("fasta", help="filtered fasta with detailed headers")
-    p.add_argument("out", help="save plot as...")
-    p.add_argument("--title", help="plot title")
     main(p.parse_args())
