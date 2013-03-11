dee7b1a670b6023d3f63d401dccc8e447419b52f
brwnj <brwnjm@gmail.com>
2013-03-11 16:53:20 -0600

    added args

--- davidson/bin/plotting.R
+++ davidson/bin/plotting.R
@@ -1,4 +1,21 @@
-#library(ggplot2)
+# in out title
+args <- commandArgs(TRUE)
 
-dt = read.table("~/projects/davidson/data/1/1.v_region_counts.txt", header=TRUE, sep="\t", row.names=1)
-barplot(dt$coverage, names.arg=rownames(dt), las=3)
+filein <- args[1]
+fileout <- args[2]
+title <- args[3]
+
+dt <- read.table(args[1],
+                header=TRUE,
+                sep="\t",
+                row.names=1)
+pdf(fileout, width=8, height=11)
+# fix bottom margin
+par(mar=c(4,10,4,2) + 0.1)
+barplot(dt$coverage,
+        names.arg=rownames(dt),
+        las=1,
+        xlab="Count",
+        main=title,
+        horiz=TRUE)
+dev.off()
