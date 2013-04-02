13ba3b6e9d7c5a546be7566ddd54976992a52da8
brwnj <brwnjm@gmail.com>
2013-04-02 12:52:55 -0600

    more samples; moved UMI here

--- polya/bin/config.sh
+++ polya/bin/config.sh
@@ -1,10 +1,11 @@
 PROJECTID=pillai_kabos_polya
 SAMPLES=(MP51 MP52 MP53 PK61 PK62 100-3 86-7 93-1 93-3 m+c              #10
             m+e2 nbt29 nbt39 nbt89 ts21 ts28 ts57 MP54 MP55 MP56        #20
-            MP57 MP58)
+            MP57 MP58 MP59 MP60 MP61 MP62 MP63)
 NOVOIDX=$HOME/projects/hits-clip/data/common/novoalign/hg18
 RESULT=$HOME/projects/polya/results/common
 DATA=$HOME/projects/polya/data/common
 CHROM_SIZES=$HOME/ref/hg18/hg18.sizes
 BIN=$HOME/projects/polya/bin
 REFBED=$DATA/polya_3utr_slop.bed
+UMI=NNNNNV
