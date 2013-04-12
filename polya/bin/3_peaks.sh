28a9712da686c6db10269651d7299278de1ef723
brwnj <brwnjm@gmail.com>
2013-04-12 13:42:13 -0600

    macs parameter changes

--- polya/bin/3_peaks.sh
+++ polya/bin/3_peaks.sh
@@ -30,8 +30,7 @@ clipped_peak=${out}_peaks.bed.clipped
 clipped_summit=${out}_summits.bed.clipped
 
 if [[ ! -f $peak ]]; then
-    macs2 callpeak -t $bam -n $out --keep-dup auto \
-        --nomodel --bw 30 --shiftsize 5 --call-summits
+    macs2 callpeak -t $bam -n $out --keep-dup auto --nomodel -s 25 --extsize 5 --call-summits
     bedClip $peak $CHROM_SIZES $clipped_peak
     bedClip $summit $CHROM_SIZES $clipped_summit
     mv $clipped_peak $peak
