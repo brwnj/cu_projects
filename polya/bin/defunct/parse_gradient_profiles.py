059a7e82386c47fe3ab147514d400a322de000d7
brwnj <brwnjm@gmail.com>
2013-04-24 10:27:47 -0600

    parsing the file may still need work

--- polya/bin/defunct/parse_gradient_profiles.py
+++ polya/bin/defunct/parse_gradient_profiles.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import sys
-import pandas as pd
-from toolshed import nopen
-import matplotlib.pyplot as plt
 
 def main(args):
-    for i, line in enumerate(nopen(args.CSV)):
-        line = line.split(",")
+    fraction = "Fraction"
+    for line in open(args.CSV, 'r'):
+        line = line.strip().split(",")
         try:
-            distance = float(line[0])
-            absorbance = float(line[1])
-            if line[2] and args.fraction:
-                if distance == 0:
-                    continue
-                t = float(line[2])
-                print "%f\t0" % (distance)
-            else:
-                print "%f\t%f" % (distance, absorbance)
-        except ValueError:
+            distance = line[0].split("(")[0]
+            absorbance = line[1]
+            try:
+                float(line[2])
+                fraction = line[2]
+            except ValueError:
+                pass
+            print "\t".join([distance, absorbance, fraction])
+        except IndexError:
             continue
 
 if __name__ == "__main__":
@@ -26,5 +22,5 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser(description=__doc__,
             formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument("CSV", help="gradient file")
-    p.add_argument("-f", "--fraction", action="store_true")
-    main(p.parse_args())
+    args = p.parse_args()
+    main(args)
