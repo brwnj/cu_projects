08d255d0e0a09a853090728ccf27ddedadb4e2cc
brwnj <brwnjm@gmail.com>
2013-04-24 10:27:15 -0600

    more samples added

--- polya/bin/config.sh
+++ polya/bin/config.sh
@@ -1,7 +1,8 @@
 PROJECTID=pillai_kabos_polya
 SAMPLES=(MP51 MP52 MP53 PK61 PK62 100-3 86-7 93-1 93-3 m+c              #10
             m+e2 nbt29 nbt39 nbt89 ts21 ts28 ts57 MP54 MP55 MP56        #20
-            MP57 MP58 MP59 MP60 MP61 MP62 MP63)
+            MP57 MP58 MP59 MP60 MP61 MP62 MP63 93-1a 86-7a 93-3a        #30
+            100-3a 89-5a 89-9a)
 NOVOIDX=$HOME/projects/hits-clip/data/common/novoalign/hg18
 RESULT=$HOME/projects/polya/results/common
 DATA=$HOME/projects/polya/data/common
