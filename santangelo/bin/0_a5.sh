2cb69f5a0a5fd299230700060e7ff22f83b66168
brwnj <brwnjm@gmail.com>
2013-04-27 19:59:04 -0600

    retrying a5

--- santangelo/bin/0_a5.sh
+++ santangelo/bin/0_a5.sh
@@ -1,9 +1,9 @@
-#! /usr/bin/env bash
-#BSUB -J a5
-#BSUB -e a5.%J.err
-#BSUB -o a5.%J.out
-#BSUB -q bigmem
-#BSUB -R "span[hosts=1]"
+#!/usr/bin/env bash
+#BSUB -J a5[1-7]
+#BSUB -o a5.%J.%I.out
+#BSUB -e a5.%J.%I.err
+#BSUB -q normal
+#BSUB -R "select[mem>16] rusage[mem=16] span[hosts=1]"
 #BSUB -n 1
 #BSUB -P santangelo
 
@@ -12,19 +12,16 @@ assemble bacterial genome using A5.
 DOC
 
 set -o nounset -o pipefail -o errexit -x
-# source $HOME/projects/santangelo/bin/config.sh
 
-# samples=(KW128)
-# sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+source $HOME/projects/santangelo/bin/config.sh
 
-sample=KW128
-reads=$HOME/projects/santangelo/data
-r1=$reads/${sample}_R1.fastq
-r2=$reads/${sample}_R2.fastq
-results=$HOME/projects/santangelo/results/common/$sample
-out=$results/$sample
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+r1=$READS/${sample}_R1.fastq.gz
+r2=$READS/${sample}_R2.fastq.gz
+results=$RESULTS/$sample
+
 if [[ ! -d $results ]]; then
     mkdir -p $results
 fi
 
-~/opt/ngopt_20130326/bin/a5_pipeline.pl $r1 $r2 $sample
+a5_pipeline.pl $r1 $r2 $sample
