ee5798193d7fe8d95d23f0e0f9acba67f6328d32
brwnj <brwnjm@gmail.com>
2013-05-31 13:42:17 -0600

    renaming peter's samples

--- polya/bin/config.sh
+++ polya/bin/config.sh
@@ -1,13 +1,14 @@
 PROJECTID=pillai_kabos_polya
-SAMPLES=(MP51 MP52 MP53 PK61 PK62 100-3 86-7 93-1 93-3 m+c              #10
-            m+e2 nbt29 nbt39 nbt89 ts21 ts28 ts57 MP54 MP55 MP56        #20
-            MP57 MP58 MP59 MP60 MP61 MP62 MP63 93-1a 86-7a 93-3a        #30
-            100-3a 89-5a 89-9a)
+SAMPLES=(MP51 MP52 MP53 PK61 PK62 PK100-3 PK86-7 PK93-1 PK93-3 PKm+c    #10
+    PKm+e2 PKnbt29 PKnbt39 PKnbt89 PKts21 PKts28 PKts57 MP54 MP55 MP56  #20
+    MP57 MP58 MP59 MP60 MP61 MP62 MP63 PK93-1a PK86-7a PK93-3a          #30
+    PK100-3a PK89-5a PK89-9a)
 NOVOIDX=$HOME/projects/hits-clip/data/common/novoalign/hg18
 RESULT=$HOME/projects/polya/results/common
 DATA=$HOME/projects/polya/data/common
 CHROM_SIZES=$HOME/ref/hg18/hg18.sizes
-BIN=$HOME/devel/worktils
-REFBED=$DATA/polya_3utr_slop.bed
+BIN=$HOME/devel/polya
+# REFBED=$DATA/polya_3utr_slop.bed
 UMI=NNNNNV
 FASTA=$HOME/ref/hg18/hg18.fa
+EXONS=$HOME/ref/hg18/refseq.exon.bed.gz
