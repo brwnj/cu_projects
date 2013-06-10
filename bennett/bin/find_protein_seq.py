23ac177ec9363b0e8ddecf674b763222ad5cd489
brwnj <brwnjm@gmail.com>
2013-06-10 10:28:51 -0600

    changes

--- bennett/bin/find_protein_seq.py
+++ bennett/bin/find_protein_seq.py
@@ -4,9 +4,10 @@
 YYC, YFC, and YSC
 """
 import sys
+from Bio import trie
 # from Bio.Seq import Seq
 from toolshed import reader
-from Bio import trie
+from itertools import ifilterfalse
 
 def get_proteins(f):
     d = {}
@@ -19,8 +20,8 @@ def get_seqs(fn):
     seqs = {}
     for l in reader(fn):
         try:
-            if not l['AA JUNCTION'].strip(): continue
-            seqs[l['Sequence ID']] = l['AA JUNCTION']
+            if not l['CDR3-IMGT'].strip(): continue
+            seqs[l['Sequence ID']] = l['CDR3-IMGT']
         except KeyError:
             continue
     return seqs
@@ -29,10 +30,10 @@ def chunker(it, n):
     # chunker('AAAABBBC', 4) --> AAAA AAAB AABB ABBB BBBC
     return [it[i:i+n] for i in xrange(0, len(it)+1-n, 1)]
 
-def build_trie(p, seqs):
+def build_trie(target, query):
     t = trie.trie()
-    lengths = sorted(set([len(v) for v in p.values()]))
-    for name, seq in seqs.iteritems():
+    lengths = sorted(set([len(v) for v in target.values()]))
+    for name, seq in query.iteritems():
         seq_len = len(seq)
         for l in lengths:
             if l > seq_len: continue
@@ -42,22 +43,41 @@ def build_trie(p, seqs):
                 t[subseq] = name
     return t
 
-def process_seqs(t, pseqs):
-    for name, pseq in pseqs.iteritems():
-        print pseq
-        print t.get_approximate(pseq, 2)
+def unique_everseen(iterable, key=None):
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
 
-def main(args):
-    proteins = get_proteins(args.cdr3)
-    seqs = get_seqs(args.seqs)
+def process_seqs(tree, query):
+    for name, seq in unique_everseen(query.iteritems(), key=lambda t: t[1]):
+        if len(seq) < 5: continue
+        print seq
+        print tree.get_approximate(seq, 2)
+
+def main(cdr3, imgtaa):
+    proteins = get_proteins(cdr3)
+    seqs = get_seqs(imgtaa)
     t = build_trie(proteins, seqs)
     process_seqs(t, proteins)
+    
+    # check if found cdr3 are subseqs of "known"
+    t = build_trie(seqs, proteins)
+    process_seqs(t, seqs)
 
 if __name__ == '__main__':
     import argparse
     p = argparse.ArgumentParser(description=__doc__,
             formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument("cdr3", help="id and protein sequence file")
-    p.add_argument("seqs", help="joined sequences")
-    args = p.parse_args()
-    main(args)
+    p.add_argument("imgtaa", help="processed, ungapped aa via high-v")
+    args = vars(p.parse_args())
+    main(**args)
--- bennett/bin/prepend_umi.py
+++ bennett/bin/prepend_umi.py
@@ -3,24 +3,24 @@
 """
 Add a piece or all of the index read back onto R1 (or R2).
 """
-from itertools import izip, ifilter, islice
+from itertools import izip, islice
 from toolshed import nopen
 
-class Fastq(object):
-    def __init__(self, args):
-        self.name = args[0][1:]
-        self.seq = args[1]
-        self.qual = args[3]
-        assert len(self.seq) == len(self.qual)
-    
-    def __repr__(self):
-        return "Fastq({name})".format(name=self.name)
-    
-    def __str__(self):
-        return "@{name}\n{seq}\n+\n{qual}".format(name=self.name,
-                seq=self.seq, qual=self.qual)
-
 def readfq(fq):
+    class Fastq(object):
+        def __init__(self, args):
+            self.name = args[0][1:]
+            self.seq = args[1]
+            self.qual = args[3]
+            assert len(self.seq) == len(self.qual)
+    
+        def __repr__(self):
+            return "Fastq({name})".format(name=self.name)
+    
+        def __str__(self):
+            return "@{name}\n{seq}\n+\n{qual}".format(name=self.name,
+                    seq=self.seq, qual=self.qual)
+    
     with nopen(fq) as fh:
         fqclean = (x.strip("\r\n") for x in fh if x.strip())
         while True:
