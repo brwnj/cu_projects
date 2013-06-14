84cb08f6fb37bcfee583c9a396a742fcc246ef3c
brwnj <brwnjm@gmail.com>
2013-06-14 13:59:59 -0600

    change reference back to old

--- pearson/bin/config.sh
+++ pearson/bin/config.sh
@@ -11,7 +11,8 @@ PAIRS=("$DATA/B1868_ATCACG_L001_R1_001.fastq.gz $DATA/B1868_ATCACG_L001_R2_001.f
         "$DATA/DisA1_TGACCA_L001_R1_001.fastq.gz $DATA/DisA1_TGACCA_L001_R2_001.fastq.gz")
 NAMES=(B1868_ATCACG_L001 DisA1_TGACCA_L001)
 # BOWTIE2_INDEX=$HOME/ref/tetrahymena/tta
-# PICARD=$HOME/opt/picard-tools-1.79
-REFERENCE=$HOME/ref/tetrahymena/Tetrahymena_thermophila.JCVI-TTA1-2.2.18.dna.toplevel.fa
+PICARD=$HOME/opt/picard-tools-1.79
+#REFERENCE=$HOME/ref/tetrahymena/Tetrahymena_thermophila.JCVI-TTA1-2.2.18.dna.toplevel.fa
+REFERENCE=$HOME/ref/tetrahymena/tta1_oct2008_finalrelease.assembly.fa
 GATK=$HOME/opt/gatk/GenomeAnalysisTK.jar
 # GSNAPINDEX=$HOME/ref/gmapdb
