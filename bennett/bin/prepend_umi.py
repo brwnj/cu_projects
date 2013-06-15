c94e030e5c5825b79e08435c6bf58969626e4afc
brwnj <brwnjm@gmail.com>
2013-06-14 22:53:28 -0600

    defaults help formatter

--- bennett/bin/prepend_umi.py
+++ bennett/bin/prepend_umi.py
@@ -13,14 +13,14 @@ def readfq(fq):
             self.seq = args[1]
             self.qual = args[3]
             assert len(self.seq) == len(self.qual)
-    
+
         def __repr__(self):
             return "Fastq({name})".format(name=self.name)
-    
+
         def __str__(self):
             return "@{name}\n{seq}\n+\n{qual}".format(name=self.name,
                     seq=self.seq, qual=self.qual)
-    
+
     with nopen(fq) as fh:
         fqclean = (x.strip("\r\n") for x in fh if x.strip())
         while True:
@@ -45,11 +45,11 @@ def main(index, fastq, begin, end):
 if __name__ == '__main__':
     import argparse
     p = argparse.ArgumentParser(description=__doc__,
-            formatter_class=argparse.RawDescriptionHelpFormatter)
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument("index", metavar="INDEX", help="index read")
     p.add_argument("fastq", metavar="FASTQ", help="R1 or R2")
     p.add_argument("-b", dest="begin", default=0, type=int,
-            help="0-based start of index read to save [%(default)s]")
+            help="0-based start of index read to save")
     p.add_argument("-e", dest="end", type=int,
             help="0-based end of index read to save")
     args = vars(p.parse_args())
