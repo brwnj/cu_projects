9162e4f0bdb0b7a68e2e029f037abde3a8b132f7
brwnj <brwnjm@gmail.com>
2013-07-12 12:33:40 -0600

    updates

---
+++ thaikoottathil/bin/0_align.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#BSUB -J align[1-11]
+#BSUB -e align.%J.%I.err
+#BSUB -o align.%J.%I.out
+#BSUB -q normal
+#BSUB -R "select[mem>12] rusage[mem=12] span[hosts=1]"
+#BSUB -n 8
+#BSUB -P thaikoottathil
+
+set -o nounset -o pipefail -o errexit -x
+source $HOME/projects/thaikoottathil/bin/config.sh
+
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+fastq=$DATA/$sample.fastq.gz
+results=$RESULTS/$sample
+if [[ ! -d $results ]]; then
+    mkdir -p $results
+fi
+bam=$results/$sample.bam
+if [ ! -f $bam ]; then
+    novoalign -d $NOVOIDX -f $fastq -o SAM -r None -c 8 -k \
+        2> $sample.alignment.txt \
+        | samtools view -ShuF4 - \
+        | samtools sort -o - $sample.temp -m 8G \
+        > $bam
+fi
---
+++ thaikoottathil/bin/config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o nounset -o
+
+SAMPLES=(10 11 12 14 15 16 18 22 23 24 3 7)
+# 1 5 9 13 19 20 21 17
+PROJECT=$HOME/projects/thaikoottathil
+RESULTS=$PROJECT/results/common
+DATA=$PROJECT/data
+NOVOIDX=$HOME/ref/hg19/hg19.9606.novoidx
