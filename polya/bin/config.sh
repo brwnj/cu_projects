7fa44226f5b3d41072af34124fec484fb7e03044
brwnj <brwnjm@gmail.com>
2013-07-19 11:35:17 -0600

    new samples

--- polya/bin/config.sh
+++ polya/bin/config.sh
@@ -3,14 +3,15 @@ SAMPLES=(MP51 MP52 MP53 MP54 MP55 MP56 MP57 MP58 MP59 MP60              #10
         MP61 MP62 MP63 PK61 PK62 PK63 PK64 PK65 PK66 PK67               #20
         PK68 PK69 PK70 PK71 PK72 PK73 PK74 PK75 PK76 PK77               #30
         PK78 PK79 PK80 PK81 PK82 PK83 PK84 PK85 PK86 PK87               #40
-        PK88 PK89 PK90 PK91 PK92)                                        #45
+        PK88 PK89 PK90 PK91 PK92 MP70 MP71 MP72 MP73 MP74               #50
+        MP75)                                                           #51
 NOVOIDX=$HOME/projects/hits-clip/data/common/novoalign/hg18
 RESULT=$HOME/projects/polya/results/common
 DATA=$HOME/projects/polya/data/common
 CHROM_SIZES=$HOME/ref/hg18/hg18.sizes
 BIN=$HOME/devel/polya
-# REFBED=$DATA/polya_3utr_slop.bed
 UMI=NNNNNV
 FASTA=$HOME/ref/hg18/hg18.fa
 EXONS=$BIN/ref/refseq.exon.bed.gz
 RUNDEXSEQ=$BIN/run_dexseq.R
+DEXRESULTS=$RESULT/dexseq_results
