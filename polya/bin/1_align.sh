85e95064ee91cff56e223d437586e418189fca05
brwnj <brwnjm@gmail.com>
2013-08-09 16:09:04 -0600

    cpu back to 12

--- polya/bin/1_align.sh
+++ polya/bin/1_align.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#BSUB -J "align[1-63]%8"
+#BSUB -J align[1-63]
 #BSUB -e align.%J.%I.err
 #BSUB -o align.%J.%I.out
 #BSUB -q normal
 #BSUB -R "select[mem>16] rusage[mem=16] span[hosts=1]"
-#BSUB -n 8
+#BSUB -n 12
 #BSUB -P pillai_kabos_polya
 
 <<DOC
@@ -37,7 +37,7 @@ stats=$results/$sample.alignment.txt
 # align the reads
 if [[ ! -f $umibam ]]; then
     # just map the reads; no iterative trimming
-    novoalign -d $NOVOIDX -f $fastq -o SAM -n 50 -r None -c 8 -k \
+    novoalign -d $NOVOIDX -f $fastq -o SAM -n 50 -r None -c 12 -k \
         2> $stats \
         | samtools view -ShuF4 - \
         | samtools sort -o - $sample.temp -m 8G \
