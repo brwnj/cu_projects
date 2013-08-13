92b1a62e703a0890b899f3910a1efe460b37eb93
brwnj <brwnjm@gmail.com>
2013-08-13 14:55:54 -0600

    new project

---
+++ davidson_transcriptome/bin/config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o nounset
+
+# 10
+SAMPLES=(1_control 1_cytokine 2_control 2_cytokine 3_control 3_cytokine 4_control 4_cytokine 5_control 5_cytokine)
+PI=davidson
+RESULTS=$HOME/projects/davidson_transcriptome/results/common
+DATA=$HOME/projects/davidson_transcriptome/data/common
+NOVOIDX=$HOME/ref/hg19/hg19.9606.novoidx
