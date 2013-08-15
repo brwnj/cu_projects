bb0f1e0939086bf81920bdd24963ff76ca249466
brwnj <brwnjm@gmail.com>
2013-08-15 17:09:02 -0600

    updates

--- polya/bin/5_run_dexseq.sh
+++ polya/bin/5_run_dexseq.sh
@@ -15,6 +15,5 @@ sample=${samples[$(($LSB_JOBINDEX - 1))]}
 counts=$RESULT/${sample}*/*.counts.txt.gz
 
 cd $DEXRESULTS
-
 # run_dexseq.py submits jobs directly into the queue
 python $BIN/run_dexseq.py -p pillai_kabos_polya -q normal $RUNDEXSEQ $counts
