d72849cc2884d677cf220a2d58c94131946e98e3
brwnj <brwnjm@gmail.com>
2013-08-20 16:13:38 -0600

    instructions on the reference

--- zhao/bin/config.sh
+++ zhao/bin/config.sh
@@ -7,4 +7,7 @@ RESULTS=$HOME/projects/zhao/results/common
 DATA=$HOME/projects/zhao/data/common
 PICARD=$HOME/opt/picard
 GATK=$HOME/opt/gatk/GenomeAnalysisTK.jar
+# need both for the reference fasta after sorting by chrom
+# samtools faidx $REFERENCE
+# java -jar ~/opt/picard/CreateSequenceDictionary.jar R=sacCer3.fa O=sacCer3.dict GENOME_ASSEMBLY=sacCer3 SPECIES=sacCer3
 REFERENCE=$HOME/ref/sacCer3/sacCer3.fa
