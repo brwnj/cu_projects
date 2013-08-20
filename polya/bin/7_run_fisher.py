f5df3fb4a1b7427ab88cb2a43904c793eb15524c
brwnj <brwnjm@gmail.com>
2013-08-20 15:39:21 -0600

    added gz extension

--- polya/bin/7_run_fisher.py
+++ polya/bin/7_run_fisher.py
@@ -15,15 +15,15 @@ script = "/vol1/home/brownj/devel/polya/fisher_test.py"
 pksamples = [s for s in os.listdir(results) if s.startswith("PK")]
 mpsamples = [s for s in os.listdir(results) if s.startswith("MP")]
 # iterate through all combinations of MP and PK samples, submitting jobs
-submit = bsub("fisher", P=projid, verbose=True)
+submit = bsub("fisher", q="short", P=projid, verbose=True)
 for strand in ["pos", "neg"]:
     # lazy
     for (a, b) in combinations(pksamples, 2):
         # write files to fisher_results
-        out = "{results}/fisher_results/{a}_to_{b}.{strand}.txt".format(**locals())
+        out = "{results}/fisher_results/{a}_to_{b}.{strand}.txt.gz".format(**locals())
         cmd = "python {script} {results}/{a}/{a}.{strand}.counts.txt.gz {results}/{b}/{b}.{strand}.counts.txt.gz | gzip -c > {out}".format(**locals())
         submit(cmd)
     for (a, b) in combinations(mpsamples, 2):
-        out = "{results}/fisher_results/{a}_to_{b}.{strand}.txt".format(**locals())
+        out = "{results}/fisher_results/{a}_to_{b}.{strand}.txt.gz".format(**locals())
         cmd = "python {script} {results}/{a}/{a}.{strand}.counts.txt.gz {results}/{b}/{b}.{strand}.counts.txt.gz | gzip -c > {out}".format(**locals())
         submit(cmd)
