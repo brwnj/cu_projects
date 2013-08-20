ba1c3c20631b123d13f0c1b50b2c06abc65abfe6
brwnj <brwnjm@gmail.com>
2013-08-20 15:48:05 -0600

    string templates

--- polya/bin/7_run_fisher.py
+++ polya/bin/7_run_fisher.py
@@ -5,6 +5,7 @@ Submit jobs for Fisher testing.
 """
 import os
 from bsub import bsub
+from string import Template
 from itertools import combinations
 
 results = "/vol1/home/brownj/projects/polya/results/common"
@@ -16,14 +17,16 @@ pksamples = [s for s in os.listdir(results) if s.startswith("PK")]
 mpsamples = [s for s in os.listdir(results) if s.startswith("MP")]
 # iterate through all combinations of MP and PK samples, submitting jobs
 submit = bsub("fisher", q="short", P=projid, verbose=True)
+out_template = Template("$results/fisher_results/${a}_to_$b.$strand.fisher.txt.gz")
+cmd_template = Template("python $script $results/$a/$a.$strand.counts.txt.gz $results/$b/$b.$strand.counts.txt.gz | gzip -c > $out")
 for strand in ["pos", "neg"]:
     # lazy
     for (a, b) in combinations(pksamples, 2):
         # write files to fisher_results
-        out = "{results}/fisher_results/{a}_to_{b}.{strand}.txt.gz".format(**locals())
-        cmd = "python {script} {results}/{a}/{a}.{strand}.counts.txt.gz {results}/{b}/{b}.{strand}.counts.txt.gz | gzip -c > {out}".format(**locals())
+        out = out_template.substitute(**locals())
+        cmd = cmd_template.substitute(**locals())
         submit(cmd)
     for (a, b) in combinations(mpsamples, 2):
-        out = "{results}/fisher_results/{a}_to_{b}.{strand}.txt.gz".format(**locals())
-        cmd = "python {script} {results}/{a}/{a}.{strand}.counts.txt.gz {results}/{b}/{b}.{strand}.counts.txt.gz | gzip -c > {out}".format(**locals())
+        out = out_template.substitute(**locals())
+        cmd = cmd_template.substitute(**locals())
         submit(cmd)
