57dcbb34cb4076e6538cd5bd0aa9512f9ea951ea
brwnj <brwnjm@gmail.com>
2013-08-22 11:11:13 -0600

    cleanup

--- polya/bin/defunct/gradient_profiles.R
+++ polya/bin/defunct/gradient_profiles.R
@@ -3,10 +3,7 @@ setwd("~/projects/polya/data/02052013/")
 
 df = read.table("new_parse_method_1.tab", header=TRUE, sep="\t")
 
-need a rectangle from 0 to the last distance of fraction == 0
-
-rect_left = c()
-rect_right = c()
+#need a rectangle from 0 to the last distance of fraction == 0
 
 positions = c(1,3,5,7,9,11,13,15,17,19,21)
 for(p in seq_along(positions)){
@@ -17,16 +14,10 @@ for(p in seq_along(positions)){
 
 min(df[df$Fraction %in% positions[5], "Distance"])
 
-
-
 p <- ggplot(df, aes(x=Distance, y=Absorbance)) + 
     geom_line() +
     theme_bw()
 p
-
-p
-
-
 
 samples=c("ctrl-2-1", "ctrl-2-2", "ctrl-2-3", "msi-9-1", "msi-9-2", "msi-9-3")
 for(i in seq_along(samples)){
