11076fde7323680e1338d2acfc6fdb70bd27a227
brwnj <brwnjm@gmail.com>
2013-08-28 20:09:49 -0600

    updates

--- bennett/bin/config.sh
+++ bennett/bin/config.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
+# 4_ATTGGC
+# 4_CACTGT
 SAMPLES=(2_ACATCG 2_CACTGT 2_CGTGAT 2_GCCTAA 2_TGGTCA           # 5
             3_ACATCG 3_CACTGT 3_CGTGAT 3_GCCTAA 3_TGGTCA        # 10
-            4_ACATCG 4_ATTGGC 4_CACTGT 4_CGTGAT 4_GATCTG        # 15
-            4_GCCTAA 4_TCAAGT 4_TGGTCA)                         # 18
+            4_ACATCG 4_CGTGAT 4_GATCTG 4_GCCTAA 4_TCAAGT        # 15
+            4_TGGTCA)                                           # 16
 PI=bennett
 RESULTS=$HOME/projects/bennett/results/common
 READS=$HOME/projects/bennett/data/common
--- bennett/bin/run.sh
+++ bennett/bin/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#BSUB -J igg_repertoire[1-18]
+#BSUB -J igg_repertoire[1-16]
 #BSUB -e igg_repertoire.%J.%I.err
 #BSUB -o igg_repertoire.%J.%I.out
 #BSUB -q normal
@@ -16,18 +16,25 @@ jobids="1"
 
 # prepend the umi back onto the reads
 i1=$READS/${sample}_I1.fastq.gz
-r1_prependumi_in=$READS/${sample}_R1.fastq.gz
-r2_prependumi_in=$READS/${sample}_R2.fastq.gz
-r1_prependumi_out=$READS/${sample}_R1.umi.fastq.gz
-r2_prependumi_out=$READS/${sample}_R2.umi.fastq.gz
+r1_in=$READS/${sample}_R1.fastq.gz
+r2_in=$READS/${sample}_R2.fastq.gz
+r1_prependumi=${r1_in/.fastq.gz/.umi.fastq.gz}
+r2_prependumi=${r2_in/.fastq.gz/.umi.fastq.gz}
+r1_sortumi=${r1_in/.fastq.gz/.umi.sorted.fastq.gz}
+r2_sortumi=${r2_in/.fastq.gz/.umi.sorted.fastq.gz}
+r1_umifiltered=${r1_in/.fastq.gz/.umi.sorted.umifiltered.fastq.gz}
+r2_umifiltered=${r2_in/.fastq.gz/.umi.sorted.umifiltered.fastq.gz}
+r1_rmadptr=${r1_in/.fastq.gz/.umi.sorted.umifiltered.rmadptr.fastq.gz}
+r2_rmadptr=${r2_in/.fastq.gz/.umi.sorted.umifiltered.rmadptr.fastq.gz}
+joined=$READS/${sample}.joined.fastq.gz
 
-if [[ ! -f $r1_prependumi_out ]]; then
-    job=$(echo "python $bin/prepend_umi.py -b 6 -e 14 $i1 $r1_prependumi_in | gzip -c > $r1_prependumi_out" | bsez prepend_umi -P $PI)
+if [[ ! -f $r1_prependumi ]]; then
+    job=$(echo "python $bin/prepend_umi.py -b 6 -e 14 $i1 $r1_in | gzip -c > $r1_prependumi" | bsez prepend_umi -P $PI)
     jobid=$(echo $job | sed -rn 's/.*<([0-9]+)>.*/\1/p')
     jobids="$jobids $jobid"
 fi
-if [[ ! -f $r2_prependumi_out ]]; then
-    job=$(echo "python $bin/prepend_umi.py -b 6 -e 14 $i1 $r2_prependumi_in | gzip -c > $r2_prependumi_out" | bsez prepend_umi -P $PI)
+if [[ ! -f $r2_prependumi ]]; then
+    job=$(echo "python $bin/prepend_umi.py -b 6 -e 14 $i1 $r2_in | gzip -c > $r2_prependumi" | bsez prepend_umi -P $PI)
     jobid=$(echo $job | sed -rn 's/.*<([0-9]+)>.*/\1/p')
     jobids="$jobids $jobid"
 fi
@@ -35,16 +42,13 @@ fi
 python -m bsub $jobids
 
 # sort the reads according to umi sequence
-r1_sortumi_out=$READS/${sample}_R1.umi.sorted.fastq.gz
-r2_sortumi_out=$READS/${sample}_R2.umi.sorted.fastq.gz
-
-if [[ ! -f $r1_sortumi_out ]]; then
-    job=$(echo "python $bin/process_umi.py sort $r1_prependumi_out $UMILENGTH | gzip -c > $r1_sortumi_out" | bsez sort_umi -P $PI)
+if [[ ! -f $r1_sortumi ]]; then
+    job=$(echo "python $bin/process_umi.py sort $r1_prependumi $UMILENGTH | gzip -c > $r1_sortumi" | bsez sort_umi -P $PI)
     jobid=$(echo $job | sed -rn 's/.*<([0-9]+)>.*/\1/p')
     jobids="$jobids $jobid"
 fi
-if [[ ! -f $r2_sortumi_out ]]; then
-    job=$(echo "python $bin/process_umi.py sort $r2_prependumi_out $UMILENGTH | gzip -c > $r2_sortumi_out" | bsez sort_umi -P $PI)
+if [[ ! -f $r2_sortumi ]]; then
+    job=$(echo "python $bin/process_umi.py sort $r2_prependumi $UMILENGTH | gzip -c > $r2_sortumi" | bsez sort_umi -P $PI)
     jobid=$(echo $job | sed -rn 's/.*<([0-9]+)>.*/\1/p')
     jobids="$jobids $jobid"
 fi
@@ -52,24 +56,16 @@ fi
 python -m bsub $jobids
 
 # collapse and filter out redundant sequences per umi
-r1_umifiltered_out=$READS/${sample}_R1.umi.sorted.umifiltered.fastq.gz
-r2_umifiltered_out=$READS/${sample}_R2.umi.sorted.umifiltered.fastq.gz
-
-if [[ ! -f $r1_umifiltered_out ]] || [[ ! -f $r2_umifiltered_out ]]; then
-    python $bin/process_umi.py collapse $r1_sortumi_out $r2_sortumi_out $UMISEQ
+if [[ ! -f $r1_umifiltered ]] || [[ ! -f $r2_umifiltered ]]; then
+    python $bin/process_umi.py collapse $r1_sortumi $r2_sortumi $UMISEQ
 fi
 
 # trim the adapters and append known primer to read name
-r1_rmadptr_out=$READS/${sample}_R1.umi.sorted.umifiltered.rmadptr.fastq.gz
-r2_rmadptr_out=$READS/${sample}_R2.umi.sorted.umifiltered.rmadptr.fastq.gz
-
-if [[ ! -f $r1_rmadptr_out ]] || [[ ! -f $r2_rmadptr_out ]]; then
-    python $BIN/trim_adapters.py $r1_umifiltered_out $r2_umifiltered_out $R1PRIMERS $R2PRIMERS
+if [[ ! -f $r1_rmadptr ]] || [[ ! -f $r2_rmadptr ]]; then
+    python $BIN/trim_adapters.py $r1_umifiltered $r2_umifiltered $R1PRIMERS $R2PRIMERS
 fi
 
 # join r1 and r2 based on local alignment of the overlap
-join_out=$READS/${sample}.joined.fastq.gz
-
-if [[ ! -f $join_out ]]; then
-    echo "python $bin/join_reads.py -t 8 $r1_rmadptr_out $r2_rmadptr_out | gzip -c > $join_out" | bsez join_reads -P $PI -n 8 -R span[hosts=1]
+if [[ ! -f $joined ]]; then
+    echo "python $bin/join_reads.py -t 8 $r1_rmadptr $r2_rmadptr | gzip -c > $joined" | bsez join_reads -P $PI -n 8 -R span[hosts=1]
 fi
