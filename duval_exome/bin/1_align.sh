765ee2291ae12ce63fd282ae27facfc807add227
brwnj <brwnjm@gmail.com>
2013-09-06 20:17:34 -0600

    updates

---
+++ duval_exome/bin/1_align.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#BSUB -J align[1-17]
+#BSUB -e align.%J.%I.err
+#BSUB -o align.%J.%I.out
+#BSUB -q normal
+#BSUB -R "select[mem>16] rusage[mem=16] span[hosts=1]"
+#BSUB -n 8
+#BSUB -P duval
+
+set -o nounset -o pipefail -o errexit -x
+source $HOME/projects/duval_exome/bin/config.sh
+
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+r1=$DATA/${sample}_R1.fastq.gz
+r2=$DATA/${sample}_R2.fastq.gz
+results=$RESULTS/$sample
+bam=$results/$sample.bam
+if [[ ! -d $results ]]; then
+    mkdir -p $results
+fi
+if [[ ! -f $bam ]]; then
+    novoalign -d $NOVOIDX -f $r1 $r2 -o SAM "@RG\tID:$sample\tSM:$sample\tPU:Illumina\tLB:PE" -r None -i 250 100 -c 8 -k \
+        2> $sample.alignment.txt \
+        | samtools view -ShuF4 - \
+        | samtools sort -o -m 8G - $sample.temp \
+        > $bam
+fi
---
+++ duval_exome/bin/config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+SAMPLES=(1025 113 130801 166408 172160 22 353GAAACC 353GGCTAC 36 400 500 522 730 735 868 Bililey KTCC)
+RESULTS=$HOME/projects/duval_exome/results/common
+DATA=$HOME/projects/duval_exome/data/common
+NOVOIDX=$HOME/ref/canis_familiaris/canis_familiaris.masked.nix
