8a94fa8e0409d8c18f71f7fe5349638e803d4b3f
brwnj <brwnjm@gmail.com>
2013-09-26 13:57:29 -0600

    normalizing counts prior to pooling

--- polya/bin/get_pool_counts.py
+++ polya/bin/get_pool_counts.py
@@ -7,9 +7,45 @@ import os
 import sys
 import gzip
 import argparse
+import numpy as np
+import pandas as pd
 from itertools import izip
 from toolshed import reader
 from collections import defaultdict
+
+def get_sample_name(fname):
+    # clip off ".counts.txt.gz"
+    return fname[:-14]
+
+def _sf_deseq(counts):
+    """
+    Calculate DESeq scaling factor per sample.
+    """
+    # masked array to discard inf, -inf, and nan
+    ma = np.ma.masked_invalid(counts)
+    return np.exp(np.ma.median(ma))
+
+def norm_deseq(df):
+    """
+    Normalize by DESeq scaling factor, which is computed as the median of
+    the ratio, for each row (gene), of its read count over its geometric
+    mean across all samples. Return new counts dataframe.
+
+    Details:
+    --------
+    http://genomebiology.com/2010/11/10/R106
+
+    Parameters:
+    -----------
+    - df: pandas dataframe.
+    """
+    # log of counts
+    lg = df.apply(np.log)
+    # per sample: exponential(median(log(counts) - geometric mean))
+    sf = lg.sub(lg.mean(axis=1), axis=0).apply(_sf_deseq, axis=0)
+    # apply scaling
+    df = df.div(sf, axis=1)
+    return df
 
 def main(count_files, metadata):
     pools = defaultdict(list)
@@ -23,11 +59,26 @@ def main(count_files, metadata):
         print >>sys.stderr, ">> processing", pool
         for strand in ["pos", "neg"]:
             files = [f for f in count_files if os.path.basename(f).split(".")[0] in samples and strand in os.path.basename(f)]
-            pool_file = gzip.open("{pool}.{strand}.txt.gz".format(pool=pool, strand=strand), "wb")
-            for count_data in izip(*[reader(f, header=False) for f in files]):
-                total_count = sum([int(c[-1]) for c in count_data])
-                print >>pool_file, "{name}\t{count}".format(name=count_data[0][0], count=total_count)
-            pool_file.close()
+            # simplest way to join files into a dataframe
+            raw_count_data = {}
+            for file_path in files:
+                sample = get_sample_name(file_path)
+                raw_count_data[sample] = {}
+                for toks in reader(file_path, header=['site', 'count']):
+                    raw_count_data[sample][toks['site']] = int(toks['count'])
+            # dataframe from dict of dicts
+            count_data = pd.DataFrame(raw_count_data)
+            # normalize the counts
+            count_data = norm_deseq(count_data)
+            # round the normalized counts up to int
+            # don't want to throw out single counts at any site
+            count_data = count_data.apply(np.ceil)
+            # sum the rows
+            count_data[pool] = count_data.sum(axis=1)
+            # print results
+            out_file = gzip.open("{pool}.{strand}.txt.gz".format(pool=pool, strand=strand), "wb")
+            count_data[pool].astype('int').to_csv(out_file, sep="\t")
+            out_file.close()
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description=__doc__,
