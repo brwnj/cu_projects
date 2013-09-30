a294b4e4d009f174ec8777fe4c6cc43901c1abe3
brwnj <brwnjm@gmail.com>
2013-09-30 12:47:58 -0600

    align using tophat2

--- davidson_transcriptome/bin/1_align.sh
+++ davidson_transcriptome/bin/1_align.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#BSUB -J align[1-10]
+#BSUB -J align[1,11]
 #BSUB -e align.%J.%I.err
 #BSUB -o align.%J.%I.out
 #BSUB -q normal
-#BSUB -R "select[mem>8] rusage[mem=8] span[hosts=1]"
+#BSUB -R "select[mem>16] rusage[mem=16] span[hosts=1]"
 #BSUB -n 8
 #BSUB -P davidson_transcriptome
 
@@ -19,9 +19,6 @@ if [[ ! -d $results ]]; then
     mkdir -p $results
 fi
 if [[ ! -f $bam ]]; then
-    novoalign -d $NOVOIDX -f $fastq -o SAM -r None -c 8 -k \
-        2> $sample.alignment.txt \
-        | samtools view -ShuF4 - \
-        | samtools sort -o - $sample.temp -m 8G \
-        > $bam
+    tophat2 -o $results -p 8 --b2-fast -G $GENES --transcriptome-index $TRANSCRIPTOME $BTBASE $fastq
+    mv $results/accepted_hits.bam $bam
 fi
