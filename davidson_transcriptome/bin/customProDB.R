f2d9972c92fa47e5d5549e41d1e1b18f09dd9be3
brwnj <brwnjm@gmail.com>
2013-10-21 13:20:54 -0600

    updated folder name

--- davidson_transcriptome/bin/customProDB.R
+++ davidson_transcriptome/bin/customProDB.R
@@ -13,25 +13,27 @@ PrepareAnnotationEnsembl(mart=ensembl, annotation_path=annotationpath, dbsnp="sn
 # annotationpath <- "~/projects/davidson_transcriptome/data/refseq"
 # PrepareAnnotationRefseq(genome="hg19", cdsfasta, pepfasta, annotationpath, dbsnp="snp137")
 
-path <- "/Users/brownj/projects/davidson_transcriptome/data/20130927/bams/"
-bamFile <- paste(path, "/", list.files(path, pattern="*bam$"), sep='')
+path <- "/Users/brownj/projects/davidson_transcriptome/data/20131021/bams"
+bams <- paste(path, "/", list.files(path, pattern="*bam$"), sep='')
 load("~/projects/davidson_transcriptome/data/ensembl/ids.RData")
 load("~/projects/davidson_transcriptome/data/ensembl/exon_anno.RData")
 load("~/projects/davidson_transcriptome/data/ensembl/proseq.RData")
 # load("~/projects/davidson_transcriptome/data/ensembl/dbsnpinCoding.RData")
 
-rpkms <- sapply(bamFile, function(x) calculateRPKM(x, exon, proteincodingonly=TRUE, ids))
+rpkms <- sapply(bams, function(x) calculateRPKM(x, exon, proteincodingonly=TRUE, ids))
 outfile <- "~/projects/davidson_transcriptome/data/20130927/rpkm_share.fasta"
 pro <- OutputsharedPro(rpkms, cutoff=10, share_sample=2, proteinseq, outfile, ids)
 
+path <- "/Users/brownj/projects/davidson_transcriptome/data/20131021/vcfs"
+vcfs <- paste(path, "/", list.files(path, pattern="*vcf$"), sep='')
+shared <- Multiple_VCF(vcfs, share_num=2)
 
 
-
-bampath <- "~/projects/davidson_transcriptome/data/20130927/bams"
-vcfpath <- "~/projects/davidson_transcriptome/data/20130927/vcfs"
+bampath <- "~/projects/davidson_transcriptome/data/20131021/bams"
+vcfpath <- "~/projects/davidson_transcriptome/data/20131021/vcfs"
 annotationpath <- "~/projects/davidson_transcriptome/data/ensembl"
-outfilepath <- "~/projects/davidson_transcriptome/data/20130927/"
+outfilepath <- "~/projects/davidson_transcriptome/data/20131021/"
 outfilename <- "davidson"
 easyRun_mul(bampath, vcfFile_path=vcfpath, annotation_path=annotationpath, 
-            rpkm_cutoff=30, share_num=3, var_share_num=2, outfile_path=outfilepath,
+            rpkm_cutoff=10, share_num=2, var_share_num=2, outfile_path=outfilepath,
             outfile_name=outfilename, INDEL=TRUE)
