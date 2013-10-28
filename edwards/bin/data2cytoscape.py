1b2030dc3eac996fce27f1ae162b9c0e33f87e12
brwnj <brwnjm@gmail.com>
2013-10-28 14:47:37 -0600

    edwards cytoscape stuff

---
+++ edwards/bin/data2cytoscape.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+parse data table into cytoscape files
+"""
+
+import argparse
+from toolshed import reader
+
+def main(table):
+    d = {}
+    for toks in reader(table, header=True):
+        d[toks['Genes']] = {}
+        for col_gene in toks.keys():
+            try:
+                d[toks['Genes']][col_gene] = int(toks[col_gene])
+            except ValueError:
+                # avoid adding the gene name as a count
+                continue
+    
+    # print node size attributes
+    node_out = open("node_size.txt", "wb")
+    print >>node_out, "source\ttotal_mutations"
+    for k in d.keys():
+        try:
+            print >>node_out, "{gene}\t{count}".format(gene=k, count=d[k][k])
+        except KeyError:
+            # this shouldn't occur but it does...
+            print >>node_out, "{gene}\t1".format(gene=k)
+    node_out.close()
+    
+    # print network and edge attributes
+    interaction_type = "pp"
+    network_out = open("network.txt", "wb")
+    print >>network_out, "source\tinteraction_type\ttarget\tcomutation_count"
+    seen = set()
+    for row_gene in d.keys():
+        for col_gene, count in d[row_gene].iteritems():
+            if count == 0: continue
+            # filter out total mutations
+            if row_gene == col_gene: continue
+            # check to see if the interaction was already added in the opposite direction
+            if "{gene2}_{gene1}".format(gene2=col_gene, gene1=row_gene) in seen: continue
+            print >>network_out, "{gene1}\t{interaction}\t{gene2}\t{count}".format(gene1=row_gene, interaction=interaction_type, gene2=col_gene, count=count)
+            seen.add("{gene1}_{gene2}".format(gene1=row_gene, gene2=col_gene))
+    network_out.close()
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(description=__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument('table')
+    args = p.parse_args()
+    main(args.table)
