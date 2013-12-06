60cf46d492f5a0336e38e96673bc1c776aef1d3c
brwnj <brwnjm@gmail.com>
2013-12-05 21:43:38 -0700

    gsnap alignment call had issues

--- leung/bin/1_align.sh
+++ leung/bin/1_align.sh
@@ -3,7 +3,7 @@
 #BSUB -e align.%J.%I.err
 #BSUB -o align.%J.%I.out
 #BSUB -q normal
-#BSUB -R "select[mem>4] rusage[mem=4] span[hosts=1]"
+#BSUB -R "select[mem>24] rusage[mem=24] span[hosts=1]"
 #BSUB -n 8
 #BSUB -P leung
 
@@ -15,7 +15,8 @@ set -o nounset -o pipefail -o errexit -x
 source $HOME/projects/leung/bin/config.sh
 
 sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
-fastq=$DATA/common/$sample.fastq.gz
+r1=$DATA/common/${sample}_R1.fastq.gz
+r2=$DATA/common/${sample}_R2.fastq.gz
 results=$RESULTS/$sample
 bam=$results/$sample.bam
 
@@ -25,9 +26,8 @@ fi
 if [[ ! -f $bam ]]; then
     gsnap -D ~analysiscore/genomes/GMAPDB/hg19_semiTotal -d hg19_semiTotal --gunzip \
         -t 8 -N 1 -s ~analysiscore/genomes/GMAPDB/hg19_semiTotal/hg19_semiTotal.maps/hg19_semiTotal.ensembl.splicesites \
-        -v ~analysiscore/genomes/GMAPDB/hg19_semiTotal/hg19_semiTotal.maps/hg19_semiTotal.snp135_strict \
         --read-group-id $sample --read-group-name $sample --read-group-library $sample --read-group-platform Illumina \
-        -Q -n 20 -A sam $fastq \
+        -B 3 -Q -n 20 -A sam $r1 $r2 \
         | samtools view -ShuF4 - \
         | samtools sort -o -m 8G - $sample.temp \
         > $bam
--- leung/bin/config.sh
+++ leung/bin/config.sh
@@ -2,5 +2,5 @@
 set -o nounset
 
 SAMPLES=(143_1_20h 143_1HSV1_20h)
-DATA=$HOME/projects/leung/data/
+DATA=$HOME/projects/leung/data
 RESULTS=$HOME/projects/leung/results/common
