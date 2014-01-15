89dc535500c734dee45216eec78b8b7c033c968e
brwnj <brwnjm@gmail.com>
2014-01-15 10:56:19 -0700

    new parameters

--- bennett/bin/cdr3_target_alignment.py
+++ bennett/bin/cdr3_target_alignment.py
@@ -21,39 +21,37 @@ def wrapper(func):
   return wrap
 IMapIterator.next = wrapper(IMapIterator.next)
 
-PW = pairwise2.align.localms
-
 class Alignment(object):
     __slots__ = ['query', 'target', 'score', 'start', 'stop']
-    
+
     def __init__(self, args):
         for a, v in zip(self.__slots__, args):
             setattr(self, a, v)
         self.score = float(self.score)
         self.start = int(self.start)
         self.stop = int(self.stop)
-    
+
     def __repr__(self):
         return "Alignment ({query} to {target})".format(query=self.query,
                 target=self.target)
-    
+
     def __str__(self):
         return "\n".join(str(getattr(self, s)) for s in self.__slots__)
-    
+
     @property
     def alignment_length(self):
         return abs(self.stop - self.start)
 
 def process_chunk(chunk):
     query, target = chunk
-    for aln in PW(query, target, 1, -1, -1, -1):
+    for aln in pairwise2.align.localms(query, target, 1, -1, -1, -1):
         return Alignment(aln), target
 
-def main(cdr3s, targets, mismatches=3, threads=1):
+def main(cdr3s, targets, mismatches, threads):
     cdr3s = [cdr3.strip("\r\n") for cdr3 in open(cdr3s, 'rb')]
     targets = [target.strip("\r\n") for target in open(targets, 'rb')]
-    threads = 6
-    p = multiprocessing.Pool(threads)    
+
+    p = multiprocessing.Pool(threads)
     for result in p.imap(process_chunk, product(cdr3s, targets), 100):
         try:
             alignment, target_seq = result
@@ -72,5 +70,11 @@ if __name__ == '__main__':
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('observed_cdr3')
     p.add_argument('targets')
-    args = p.parse_args()
-    main(args.observed_cdr3, args.targets)
+    p.add_argument("-m", "--mismatches", type=int, default=3)
+    p.add_argument("-t", "--threads", type=int, default=1)
+    args = vars(p.parse_args())
+
+    cpus = multiprocessing.cpu_count()
+    args['threads'] = cpus if args['threads'] > cpus
+
+    main(**args)
