8776ac48d285f6bf1f85982f46d23519a5b5b8fa
brwnj <brwnjm@gmail.com>
2014-01-21 10:41:40 -0700

    fix trimming by track name

--- hits-clip/bin/scripts/trim_peaks.py
+++ hits-clip/bin/scripts/trim_peaks.py
@@ -1,122 +1,119 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Trim peaks by finding the point of maximum intensity then expanding to the
-size of width.
-
-Determines which tracks will be used for trimming based on file name and has 
-assumptions about your genomedata archive:
-
-1. your genomedata archive only has this sample once
-2. sample names terminate with a period character, then list strand, e.g. 
-MP1.pos.rmd
-<sample>.<strand>.<...>
-
-The assumptions concerning your peaks file is that it starts with the sample name
-and is delimited by periods:
-
-MP1.peaks.rmd.bed
-<sample>.peaks.<...>
+Trim peaks by finding the point of maximum intensity for a given interval within
+the genomedata archive then expanding to `width`.
 """
 
 import os
 import sys
 import warnings
+from numpy import nansum
 from genomedata import Genome
 from pybedtools import BedTool
-from numpy import nansum
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-def trim_peaks(bed, genomedata, track_idx, peak_ext, verbose):
-    """For each peak, find mid then write new coordinates from the mid."""
-    bed = BedTool(bed)
-    with Genome(genomedata) as gd:
-        for chrom in gd:
-            if verbose: sys.stderr.write(">> processing %s...\n" % chrom)
-            for supercontig, continuous in chrom.itercontinuous():
-                whole_tracks = {}
-                # track_idx = {"+":1,"-":0}
-                for strand, idx in track_idx.iteritems():
-                    whole_tracks[strand] = continuous[:, idx]
-                # will be iterating over this for each supercontig
-                for b in bed:
-                    # speed up processing
-                    if b.chrom != chrom.name: continue
-                    # only test the correct peaks
-                    # should work even if not sorted
-                    if not b.start >= supercontig.start: continue
-                    if not b.stop <= supercontig.end: continue
-                    # get the relative position since continous will start at 0
-                    relative_start = supercontig.project(b.start)
-                    relative_stop = supercontig.project(b.stop)
-                    # counts are only taken from appropriate strand
-                    counts = whole_tracks[b.strand][relative_start:relative_stop]
-                    # pass the actual start since we're trimming the peak here
-                    mid = get_mid(b.start, counts)
-                    start = mid - peak_ext
-                    stop = mid + peak_ext
-                    fields = [b.chrom, start, stop, b.name, b.score, b.strand]
-                    print "\t".join(map(str, fields))
 
 def get_mid(start, counts):
     """Finds median position of the maximum intensity value."""
+
     maximum = 0
     maxpositions = []
+
     # get all of the positions with the max intensity
     for idx, count in enumerate(counts, start=start):
-        if count < maximum: continue
-        if count == maximum:
+        summed = nansum(count)
+        if summed < maximum: continue
+        if summed == maximum:
             maxpositions.append(idx)
             continue
-        if count > maximum:
-            maximum = count
+        if summed > maximum:
+            maximum = summed
             del maxpositions[0:len(maxpositions)]
             maxpositions.append(idx)
-    # return midpoint among the max intensities
-    return int(maxpositions[len(maxpositions) / 2])
 
-def get_track_idx(gd, sample, verbose):
-    """returns dict of strand:index"""
-    indexes = {}
-    # parse the file name
-    sample = sample.split(".peaks")[0]
+    # return midpoint among the max intensities
+    midpoint = int(maxpositions[len(maxpositions) / 2])
+
+    return midpoint
+
+
+def indexes_from_archive(genomedata, track_names, verbose):
+    """returns list of indexes."""
+    if verbose:
+        print >>sys.stderr, ">> retrieving track indexes"
+    indexes = []
     with Genome(gd) as genome:
-        for i, track in enumerate(genome.tracknames_continuous):
-            # t_sample, strand, extraneous = track.split(".", 2)
-            if "neg" in track:
-                t_sample = track.split(".neg")[0]
-                strand = "-"
-            else:
-                t_sample = track.split(".pos")[0]
-                strand = "+"
-            if sample == t_sample:
-                if verbose: sys.stderr.write(">> found track: %s\n" % track)
-                indexes[strand] = i
-    # should only have one pos and one neg for a sample
-    assert(len(indexes.keys()) == 2)
+        for track_name in track_names:
+            try:
+                idx = genome.index_continuous(track_name)
+                indexes.append(idx)
+            except KeyError:
+                print >>sys.stderr, "invalid track name:", track_name
+                sys.exit(1)
     return indexes
 
-def main(args):
+
+def main(peaks, genomedata, tracks, width, verbose, warnings):
     # pytables via genomedata raises warn PerformanceWarning
-    if not args.warnings:
+    if not warnings:
         warnings.simplefilter("ignore")
-    track_idx = get_track_idx(args.genomedata, os.path.basename(args.peaks), args.verbose)
-    kwargs = {'bed':args.peaks,
-              'genomedata':args.genomedata,
-              'track_idx':track_idx,
-              'peak_ext':args.width / 2,
-              'verbose':args.verbose}
-    trim_peaks(**kwargs)
+
+    track_indexes = indexes_from_archive(genomedata, tracks, verbose)
+
+    # half of width to extend start and stop
+    from_start = width / 2
+    from_stop = from_start + 1
+
+    bed = BedTool(bed)
+
+    with Genome(genomedata) as gd:
+        for chrom in gd:
+            if verbose:
+                print >>sys.stderr, ">> processing {chrom}...".format(chrom=chrom)
+
+            for supercontig, continuous in chrom.itercontinuous():
+
+                track_continuous = continuous[:, [track_indexes]]
+
+                # will be iterating over this for each supercontig
+                for b in bed:
+                    # there must be a better way...
+                    if b.chrom != chrom.name: continue
+
+                    # only trim within this supercontig
+                    if not b.start >= supercontig.start: continue
+                    if not b.stop <= supercontig.end: continue
+
+                    # get the relative position since continous will start at 0
+                    relative_start = supercontig.project(b.start)
+                    relative_stop = supercontig.project(b.stop)
+
+                    # count slice for bed region
+                    counts = track_continuous[relative_start:relative_stop]
+
+                    # pass the actual start since we're trimming the peak here
+                    mid = get_mid(b.start, counts)
+                    start = mid - from_start
+                    stop = mid + from_stop
+                    print "{chrom}\t{start}\t{stop}\t{name}\t{score}\t{strand}".format(
+                            chrom=b.chrom, start=start, stop=stop, name=b.name,
+                            score=b.score, strand=b.strand)
+
 
 if __name__ == "__main__":
-    import argparse
-    p = argparse.ArgumentParser(description=__doc__, 
-                    formatter_class=argparse.RawDescriptionHelpFormatter)
-    p.add_argument('peaks', help='sorted peaks file (bed)')
-    p.add_argument('genomedata', help="genome data archive")
+    p = ArgumentParser(description=__doc__,
+                    formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('peaks', help='sorted peaks as bed')
+    p.add_argument('genomedata', help="genomedata archive")
+    p.add_argument("-t", "--trackname", action="append", required=True,
+                    help=("track names for samples comprising the replicates"
+                            "in the peaks bed"))
     p.add_argument("-w", "--width", default=50, type=int,
-                    help="full peak width [ %(default)s ]")
+                    help="full peak width")
     p.add_argument("-v", "--verbose", action='store_true',
-                    help="maximum verbosity [ %(default)s ]")
+                    help="maximum verbosity")
     p.add_argument("--warnings", action='store_true',
-                    help="show warnings [ %(default)s ]")
-    main(p.parse_args())
+                    help="show warnings")
+    args = vars(p.parse_args())
+    main(**args)
