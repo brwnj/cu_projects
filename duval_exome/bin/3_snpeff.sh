e38ffe3cfd700ef30f31893859511d57d3e3216b
brwnj <brwnjm@gmail.com>
2014-01-31 14:46:57 -0700

    duval

--- duval_exome/bin/3_snpeff.sh
+++ duval_exome/bin/3_snpeff.sh
@@ -14,18 +14,22 @@ sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 
 vcf=$RESULTS/$sample/$sample.vcf
 snpeff=$RESULTS/$sample/$sample.snpeff.txt.gz
-annotated=$RESULTS/$sample/$sample.annotated_snps.txt.gz
+# annotated=$RESULTS/$sample/$sample.annotated_snps.txt.gz
 
-if [[ ! -f $annotated ]]; then
-    java -jar $SNPEFF eff -chr chr -noStats -v -c $SNPEFFCONFIG -o txt canFam3 $vcf | gzip -c > $snpeff
-    python $BIN/annotate.py $snpeff $ANNOTATION | gzip -c > $annotated
-fi
-
-vcf=$RESULTS/$sample/$sample.freebayes.vcf
-snpeff=$RESULTS/$sample/$sample.snpeff.freebayes.txt.gz
-annotated=$RESULTS/$sample/$sample.annotated_snps.freebayes.txt.gz
-
-if [[ ! -f $annotated ]]; then
-    java -jar $SNPEFF eff -chr chr -noStats -v -c $SNPEFFCONFIG -o txt -minQ 10 -minC 10 -no-downstream -no-intergenic -no-intron -no-upstream canFam3 $vcf | gzip -c > $snpeff
-    python $BIN/annotate.py $snpeff $ANNOTATION | gzip -c > $annotated
+if [[ ! -f $snpeff ]]; then
+    java -jar $SNPEFF eff \
+        -chr chr \
+        -fi $TARGETS \
+        -minC 10 \
+        -no-downstream \
+        -no-intergenic \
+        -no-intron \
+        -no-upstream \
+        -noStats \
+        -v \
+        -c $SNPEFFCONFIG \
+        -o txt \
+        canFam3 $vcf \
+        | gzip -c > $snpeff
+    # python $BIN/annotate.py $snpeff $ANNOTATION | gzip -c > $annotated
 fi
