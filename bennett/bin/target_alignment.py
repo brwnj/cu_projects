0ff884e633a135b34f4ed96f4ebed4e05c8d8f72
brwnj <brwnjm@gmail.com>
2014-02-04 10:49:38 -0700

    bennett

--- bennett/bin/target_alignment.py
+++ bennett/bin/target_alignment.py
@@ -7,12 +7,28 @@ Align observed CDR3 sequences to target sequences obtained outside of NGS.
 import os
 import sys
 import pandas as pd
-from Bio import pairwise2
+import editdist as ed
 from toolshed import reader
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
+def distance(a, b):
+    """
+    find best edit distance between two strings of potentially uneven length.
+    """
+    la, lb = len(a), len(b)
+    assert isinstance(a, basestring), isinstance(b, basestring)
+    if la < lb:
+        return distance(b, a)
+    if la == lb:
+        return ed.distance(a, b)
+    else:
+        dists = []
+        for i in xrange(0, la-lb+1):
+            dists.append(ed.distance(a[i:i+lb], b))
+        return min(dists)
 
-def main(queries, targets, identity, shortest):
+
+def main(queries, targets, imgt_column, identity, length):
     query_sets = {}
 
     for query_file in queries:
@@ -21,17 +37,16 @@ def main(queries, targets, identity, shortest):
 
         for toks in reader(query_file):
             if not toks['Functionality'] == "productive": continue
-            if not len(toks['CDR3-IMGT']) >= shortest: continue
 
             # read_7:AAAAAAAT:Cm2:VH3Fr1
             vh_family = toks['Sequence ID'].rsplit(":", 1)[1]
             # only care about unique CDR3s
-            query_sets[sample_id][toks['CDR3-IMGT']] = {'vdj':toks['V-D-J-REGION'], 'vh_family':vh_family}
+            query_sets[sample_id][toks[imgt_column]] = {'vdj':toks['V-D-J-REGION'], 'vh_family':vh_family}
 
     # process target sequences
     target_df = pd.read_table(targets)
 
-    # data['patient']['peptide_source:peptide_sequence:miseq_result:alignment:vdj:vh_family'] = 1
+    # data['id']['peptide_src:peptide_seq:miseq_res:vdj:vh_family'] = 1
     data = {}
 
     for sample_id, cdr3s in query_sets.iteritems():
@@ -52,34 +67,39 @@ def main(queries, targets, identity, shortest):
 
                     query_length = len(query)
 
+                    # only compare sequences that are similar length
+                    if abs(query_length - target_length) > length: continue
+
+                    # find shortest for identity calculation
                     shortest = float(min([target_length, query_length]))
 
-                    for a_query, a_target, score, start, stop in pairwise2.align.localms(query, target, 1, -1, -5, -1):
-                        if score / shortest > identity:
-                            # add matched alignment
-                            # when new patient shows up, will likely not overlap
-                            # do to all these things being in the key
-                            k = "{source}:{known}:{miseq}:{alignment}:{vdj}:{vh}".format(\
-                                    source=source,
-                                    known=target,
-                                    miseq=query,
-                                    alignment=a_query,
-                                    vdj=toks['vdj'],
-                                    vh=toks['vh_family'])
-                            data[sample_id][k] = 1
+                    # find edit distance accounting for length disparity
+                    d = distance(query, target)
+
+                    # add matched alignment
+                    if ((shortest - d) / shortest) > identity:
+                        k = "{source}:{known}:{miseq}:{vdj}:{vh}".format(\
+                                source=source,
+                                known=target,
+                                miseq=query,
+                                vdj=toks['vdj'],
+                                vh=toks['vh_family'])
+                        data[sample_id][k] = 1
 
     df = pd.DataFrame(data)
     df.index = pd.MultiIndex.from_tuples([x.split(":") for x in df.index], \
-                names=['peptide_source', 'peptide_sequence', 'miseq_result', 'alignment', 'VDJ', 'VH'])
+                names=['peptide_source', 'peptide_sequence', 'miseq_result', 'VDJ', 'VH'])
     df.to_csv(sys.stdout, sep="\t", na_rep="0")
+
 
 if __name__ == '__main__':
     p = ArgumentParser(description=__doc__,
             formatter_class=ArgumentDefaultsHelpFormatter)
     p.add_argument('-q', '--queries', nargs="+", help="IMGT AA sequence files -- should be 5_*.txt")
     p.add_argument('-t', '--targets', help="target sequence file with peptide sequences per column, header with source id")
+    p.add_argument("-c", "--imgt-column", default="CDR3-IMGT", help="column name for sequences")
     p.add_argument("-i", "--identity", type=float, default=0.75, help="sequence identity threshold -- number identical divided by length of the shorter sequence")
-    p.add_argument("-s", "--shortest", type=int, default=6, help="shortest allowable peptide sequence")
+    p.add_argument("-l", "--length", type=int, default=2, help="allowable length difference between target and query sequence")
     args = vars(p.parse_args())
 
     main(**args)
