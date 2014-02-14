0c9fdfd7b50cf550a156a495e704590a5339a7b8
brwnj <brwnjm@gmail.com>
2014-02-14 11:53:55 -0700

    associative array for coloring tracks

--- duval_exome/bin/config.sh
+++ duval_exome/bin/config.sh
@@ -15,3 +15,23 @@ TARGETS=$HOME/ref/canFam3/canFam3.SureDesign.bed.gz
 NORMALVCF=$RESULTS/normal.vcf
 HUB=$RESULTS/hub
 SIZES=$HOME/ref/canFam3/canFam3.sizes
+
+declare -A HUBCOLORS
+HUBCOLORS=(
+[1025]="35,139,69"
+[113]="136,65,157"
+[166408]="43,140,190"
+[172160]="215,48,31"
+[22]="5,11,176"
+[353GAAACC]="2,129,138"
+[353GGCTAC]="206,18,86"
+[36]="174,1,126"
+[400]="35,132,67"
+[500]="34,94,168"
+[522]="204,76,2"
+[730]="227,26,28"
+[735]="102,194,164"
+[868]="140,150,198"
+[Bililey]="123,204,196"
+[KTCC]="252,141,89"
+)
