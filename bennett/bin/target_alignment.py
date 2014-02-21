224501c44cb1f4dc9f9ab534d8d33a2e5b76f1d4
brwnj <brwnjm@gmail.com>
2014-02-20 17:19:01 -0700

    getting closer

--- bennett/bin/target_alignment.py
+++ bennett/bin/target_alignment.py
@@ -28,67 +28,84 @@ def distance(a, b):
         return min(dists)
 
 
-def main(queries, targets, imgt_column, identity, length):
-    query_sets = {}
+def main(queries, targets, identity, length):
+    """
+    read in all aa sequences from one sample into table
+    run alignments
+    print output as separate files
+    """
 
-    for query_file in queries:
-        print >>sys.stderr, "reading in", query_file
-        sample_id = os.path.basename(query_file).split("_", 2)[-1].rsplit("_", 1)[0]
-        query_sets[sample_id] = {}
+    query = queries[0]
 
-        for toks in reader(query_file):
-            if not toks['Functionality'] == "productive": continue
+    # import into a table
+    df = pd.read_table(query, index_col=0, header=0, usecols=[0,1,2,3,4,6,14],
+                        names=["seq_num", "seq_id", "functionality", "v_gene",
+                                "j_gene", "vdj_seq", "imgt_cdr3"])
 
-            # read_7:AAAAAAAT:Cm2:VH3Fr1
-            vh_family = toks['Sequence ID'].rsplit(":", 1)[1]
-            # only care about unique sequences
-            if toks[imgt_column]:
-                query_sets[sample_id][toks[imgt_column]] = {'vh_family':vh_family}
+    print >>sys.stderr, "munging query data for", query
+    # drop unproductive translations
+    df = df[df.functionality == "productive"]
+    # only take one translated germline
+    df['v_gene'] = df['v_gene'].apply(lambda x: pd.Series(x.split(" ", 2)[1]))
+    df['j_gene'] = df['j_gene'].apply(lambda x: pd.Series(x.split(" ", 2)[1]))
+    # primer germline
+    df['primer_germline'] = df['seq_id'].apply(lambda x: pd.Series(x.rsplit(":", 1)[1]))
+    df['read_id'] = df['seq_id'].apply(lambda x: pd.Series(x.split(":", 1)[0]))
+    df['umi'] = df['seq_id'].apply(lambda x: pd.Series(x.split(":")[1]))
 
-    # process target sequences
+    # calculate edit distances for all unique CDR3s
+    print >>sys.stderr, "calculating match scores"
     target_df = pd.read_table(targets)
-
-    # data['id']['peptide_src:peptide_seq:miseq_res:vdj:vh_family'] = 1
-    data = {}
-
-    for sample_id, sequences in query_sets.iteritems():
-
-        print >>sys.stderr, "processing peptides for", sample_id
-        data[sample_id] = {}
-
+    alignments = {}
+    seq_to_source = {}
+    for query, grouped_df in df.groupby('imgt_cdr3'):
+        query_length = len(query)
+        alignments[query] = {}
         for source, series in target_df.iteritems():
-
+            series = series.drop_duplicates()
             for idx, target in series.iteritems():
-
+                # columns contain uneven number of rows
                 if not isinstance(target, basestring): continue
+
+                # handle duplicates but fail on identical from separate sources
+                if target in seq_to_source and source != seq_to_source[target]:
+                    print >>sys.stderr, "ambigous target", target, source
+                    sys.exit(1)
+
+                seq_to_source[target] = source
+
                 target_length = len(target)
 
-                for query, toks in sequences.iteritems():
-                    # if not isinstance(query, basestring): continue
-                    query_length = len(query)
+                # length too long
+                if length != -1 and abs(query_length - target_length) > length:
+                    alignments[query][target] = 0
+                    continue
 
-                    # only compare sequences that are similar length
-                    if length != -1 and abs(query_length - target_length) > length: continue
+                # length in range
+                shorter = float(min([target_length, query_length]))
+                d = distance(query, target)
+                match_score = (shorter - d) / shorter
+                alignments[query][target] = match_score
 
-                    # find shortest for identity calculation
-                    shortest = float(min([target_length, query_length]))
+    # print the sequences
+    header = ["read_id", "umi", "csf_cdr3", "imgt_cdr3", "match_score",
+                "csf_source", "primer_germline", "v_gene", "vdj_seq"]
 
-                    # find edit distance accounting for length disparity
-                    d = distance(query, target)
+    # groupby imgt_cdr3, primer_germline, v_gene, vdj_seq
+    for (imgt_cdr3, primer_germline, v_gene, vdj_seq), (dframe) in \
+            df.groupby(['imgt_cdr3', 'primer_germline', 'v_gene', 'vdj_seq']):
 
-                    # add matched alignment
-                    if ((shortest - d) / shortest) > identity:
-                        k = "{source}:{known}:{miseq}:{vh}".format(\
-                                source=source,
-                                known=target,
-                                miseq=query,
-                                vh=toks['vh_family'])
-                        data[sample_id][k] = 1
+        # compile list of read_ids and umis
+        reads = list(dframe['read_id'])
+        umis = list(dframe['umi'])
 
-    df = pd.DataFrame(data)
-    df.index = pd.MultiIndex.from_tuples([x.split(":") for x in df.index], \
-                names=['source', 'source_sequence', imgt_column, 'VH'])
-    df.to_csv(sys.stdout, sep="\t", na_rep="0")
+        # look up all alignments, print where above threshold
+        for csf_cdr3, match_score in alignments[imgt_cdr3].iteritems():
+            if match_score > identity:
+                fields = [",".join(reads), ",".join(umis), csf_cdr3, imgt_cdr3,
+                            match_score, seq_to_source[csf_cdr3],
+                            primer_germline, v_gene, vdj_seq]
+                print "\t".join([str(f) for f in fields])
 
 
 if __name__ == '__main__':
@@ -96,9 +113,8 @@ if __name__ == '__main__':
             formatter_class=ArgumentDefaultsHelpFormatter)
     p.add_argument('-q', '--queries', nargs="+", help="IMGT AA sequence files -- should be 5_*.txt")
     p.add_argument('-t', '--targets', help="target sequence file with peptide sequences per column, header with source id")
-    p.add_argument("-c", "--imgt-column", default="CDR3-IMGT", help="column name for sequences")
-    p.add_argument("-i", "--identity", type=float, default=0.75, help="sequence identity threshold -- number identical divided by length of the shorter sequence")
-    p.add_argument("-l", "--length", type=int, default=2, help="allowable length difference between target and query sequence -- -1 to disable")
+    p.add_argument('-i', '--identity', type=float, default=0.75, help="sequence identity threshold -- number identical divided by length of the shorter sequence")
+    p.add_argument('-l', '--length', type=int, default=2, help="allowable length difference between target and query sequence -- -1 to disable")
     args = vars(p.parse_args())
 
     main(**args)
