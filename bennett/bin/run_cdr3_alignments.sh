9147e888459f489f6544d19d8a0883e6dc2dadab
brwnj <brwnjm@gmail.com>
2014-02-20 17:18:50 -0700

    just run the .50

--- bennett/bin/run_cdr3_alignments.sh
+++ bennett/bin/run_cdr3_alignments.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#BSUB -J cdr3_local_alignment[1-4]
-#BSUB -e cdr3_local_alignment.%J.%I.err
-#BSUB -o cdr3_local_alignment.%J.%I.out
+#BSUB -J cdr3_local_alignment
+#BSUB -e cdr3_local_alignment.%J.err
+#BSUB -o cdr3_local_alignment.%J.out
 #BSUB -q normal
 #BSUB -R "select[mem>4] rusage[mem=4] span[hosts=1]"
 #BSUB -n 1
@@ -10,9 +10,4 @@
 set -o nounset -o pipefail -o errexit -x
 source $HOME/projects/bennett/bin/config.sh
 
-mismatches=(".85" ".75" ".65" ".50")
-mismatch=${mismatches[$(($LSB_JOBINDEX - 1))]}
-
-for l in 2 3 4 5; do
-    python $HOME/projects/bennett/bin/target_alignment.py -l $l -i $mismatch -q $RESULTS/5_*.txt -t $RESULTS/peptide_seqs.tsv > $RESULTS/alignments.m${mismatch}.l$l.tsv
-done
+python $HOME/projects/bennett/bin/target_alignment.py -l 2 -i .50 -q $RESULTS/5_*.txt -t $RESULTS/peptide_seqs.tsv > $RESULTS/alignments.m50.l2.tsv
