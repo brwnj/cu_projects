05028ee2090b3e3b932d8239e06a48a03d93aa43
brwnj <brwnjm@gmail.com>
2014-03-27 09:18:46 -0600

    leung

---
+++ leung/bin/gene_selection.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+"""
+import sys
+import pandas as pd
+from toolshed import reader
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+
+def main(files, min_coverage=10, gene=None, split_sep="_"):
+    tables = []
+
+    for f in files:
+        sample = f.split(split_sep)[0]
+        df = pd.read_table(f, header=2, compression="gzip")
+        df['Sample'] = sample
+        tables.append(df)
+
+    df = pd.concat(tables)
+
+    # filter
+    df = df[(df['Effect'] == "NON_SYNONYMOUS_CODING")\
+                & (df['Coverage'] >= 10)\
+                & (df['Change_type'] == "SNP")]
+    df.dropna(how="all", inplace=True)
+    if gene:
+        # print only selected genes
+        df.set_index("Gene_ID", inplace=True)
+        df = df.ix[gene]
+        df.dropna(how="all", inplace=True)
+        df.to_csv(sys.stdout, index=False, na_rep="NA")
+
+    else:
+        # print all
+        df.to_csv(sys.stdout, index=False, na_rep="NA")
+
+
+if __name__ == '__main__':
+    p = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('files', nargs="+")
+    p.add_argument('-g', '--gene', action="append", help="genes to output; empty for all")
+    p.add_argument('--min-coverage', default=10, type=int, help="minimum allowable coverage")
+    p.add_argument('--split-sep', help="split pattern on file name to grab sample name")
+    # p.add_argument('-')
+    args = vars(p.parse_args())
+    main(**args)
