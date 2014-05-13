09260fd378f24148de41e13d6fda318bcf728e85
brwnj <brwnjm@gmail.com>
2014-05-13 10:11:08 -0600

    still need to handle reps

--- hits-clip/bin/config.sh
+++ hits-clip/bin/config.sh
@@ -28,6 +28,7 @@ GENOMEDATA=$RESULTS/hitsclip.gd
 AGOFOOTPRINT=60
 HUB=$RESULTS/hub
 GENOME=hg19
+HUBNAME=HITSCLIP_201405
 
 TRIMSCRIPT=$SRC/trim_adapter.py
 TRIMADAPTER=GTGTCA
@@ -41,23 +42,47 @@ PEAKSUNIQUEQ=0.01
 declare -A REPLICATES
 REPLICATES=([BT474]="PK21 PK41 PK52"
 		[BT474estr]="PK22 PK53"
-		[BT474herc]="PK23"
 		[HELA]="helaa helab"
 		[MCF7]="PK11 PK31 PK51"
 		[MCF7estr]="PK12 PK32"
 		[MDA231]="PK24 PK42 PK54"
-		[MCF7_E_72]="PK61"
-		[MCF7_TAM_72]="PK62"
-		[MCF7_TAMRES]="PK63"
-		[MCF10A_group]="MCF10A"
-		[PK12-0_group]="PK12-0"
-		[PK12-6_group]="PK12-6"
-		[PK12-24_group]="PK12-24")
+)
 
-# PK33
-# MCF7-0
-# MCF7-6
-# MCF7-24
-# MCF7-0_CAC
-# MCF7-24_GAT
-# MCF7-6_ATT
+declare -A COLORS
+COLORS=([PK11]="0,109,44"
+        [PK31]="35,139,69"
+        [PK51]="65,174,118"
+        [MCF7]="0,68,27"
+        [PK21]="8,81,156"
+        [PK41]="33,113,181"
+        [PK52]="66,146,198"
+        [BT474]="8,48,107"
+        [PK24]="166,54,3"
+        [PK42]="217,72,1"
+        [PK54]="241,105,19"
+        [MDA231]="127,39,4"
+		[PK12]="63,0,125"
+		[PK22]="8,29,88"
+		[PK23]="73,0,106"
+		[PK32]="221,52,151"
+		[PK33]="239,59,44"
+		[PK53]="188,189,220"
+		[helaa]="103,169,207"
+		[helab]="54,144,192"
+		[PK61]="254,178,76"
+		[PK62]="199,233,180"
+		[PK63]="247,129,191"
+		[MCF10A]="122,1,119"
+		[PK12-0]="204,76,2"
+		[PK12-24]="153,52,4"
+		[PK12-6]="102,37,6"
+		[MCF7-0]="231,41,138"
+		[MCF7-6]="173,221,142"
+		[MCF7-24]="128,125,186"
+		[MCF7-0_CAC]="231,41,138"
+		[MCF7-24_GAT]="128,125,186"
+		[MCF7-6_ATT]="173,221,142"
+		[MCF7-0_TGT]="231,41,138"
+		[MCF7-24_CTG]="128,125,186"
+		[MCF7-6_GGC]="173,221,142"
+)
