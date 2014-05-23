be38a8bd1f3bec01483c32f7a6864da286f5af01
brwnj <brwnjm@gmail.com>
2014-05-23 10:22:06 -0600

    no major changes

--- hits-clip/bin/scripts/filter_sif.py
+++ hits-clip/bin/scripts/filter_sif.py
@@ -15,32 +15,31 @@ __author_email__ = "brwnjm@gmail.com"
 def get_dict(attrs_file):
     """Return dictionary of miRNA abundance."""
     mir_filter = {}
-    for a in reader(attrs_file, header="mir op value".split(), sep=' '):
-        try:
-            mir_filter[a['mir']] = a['value']
-        except KeyError:
-            pass
+    for toks in reader(attrs_file, header=["mir", "op", "value"], sep=' '):
+        mir_filter[toks['mir']] = float(toks['value'])
     return mir_filter
 
 
 def main():
-    p = argparse.ArgumentParser(description=__doc__,
-            formatter_class=argparse.RawDescriptionHelpFormatter)
-    p.add_argument('sif', help='unfiltered network (sif)')
+    p = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    p.add_argument('sif', help='unfiltered network (.sif)')
     p.add_argument('attrs', help='Cytoscape attributes file for abundance')
     args = p.parse_args()
-    
+
     mirna_filter = get_dict(args.attrs)
-    
+
     for a in reader(args.sif, header="mir length gene".split(), sep='\t'):
         try:
-            m_filter = mirna_filter[a['mir']] is "0.0"
+            m_filter = mirna_filter[a['mir']] == 0
         except KeyError:
             # header or miRNA present in the mature.fa but not the gff3, ignore for now
             continue
-        if m_filter: continue
-        fields = (a['mir'], a['length'], a['gene'])
-        print "\t".join(map(str, fields))
+        try:
+            if mirna_filter[a['mir']] == 0: continue
+        except KeyError:
+            continue
+        else:
+            print "\t".join([a['mir'], a['length'], a['gene']])
 
 
 if __name__ == "__main__":
